ca6b1ca39f5cce5bb71fede4207740f783874c9a
Eugene Bekker <ebekker@users.noreply.github.com>
2019-03-07 23:12:17 -0500

    extending containers to be  container items, starting validation testing

--- Playground1/Playground1/Components/Pages/IContainer.cs
+++ Playground1/Playground1/Components/Pages/IContainer.cs
@@ -8,19 +8,12 @@ namespace Playground1.Components.Pages
 {
     public interface IContainer
     {
-        //void Add(IContainedItem item);
-        //void Remove(IContainedItem item);
-
         RenderFragment<IContainedItem> GetTemplate();
     }
 
     public interface IContainedItem
     {
-        //RenderFragment GetContent();
-
         string GetLabel { get; set; }
-
-        string GetValue { get; set; }
 
         RenderFragment GetContent { get; set; }
     }
---
+++ Playground1/Playground1/Components/Pages/LocalModel1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Playground1.Components.Pages
+{
+    public class LocalModel1
+    {
+        [StringLength(5)]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        public int Age { get; set; }
+    }
+}
