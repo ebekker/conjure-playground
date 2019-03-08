45a706a9dabc6c988462353bcb8b94e16f864b92
Eugene Bekker <ebekker@users.noreply.github.com>
2019-03-08 16:23:07 -0500

    listbox control

---
+++ Playground/Conjure.BlazorUI/Controls/ListBox.razor.cs
@@ -0,0 +1,26 @@
+﻿using Conjure.BlazorUI.Components;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Conjure.BlazorUI.Controls
+{
+    public class _ListBox<T> : ComponentBase
+    {
+        [Parameter]
+        protected string Label { get; set; }
+
+        [Parameter]
+        protected LabelPosition? LabelPosition { get; set; }
+
+        [Parameter]
+        protected T Value { get; set; }
+
+        [Parameter]
+        protected EventCallback<T> ValueChanged { get; set; }
+
+        [Parameter]
+        protected RenderFragment ChildContent { get; set; }
+    }
+}
